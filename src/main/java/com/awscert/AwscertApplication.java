56a56f7a957b7d90ca835692c8bf729bc997a9ff
vahapgencdal <avahap19@gmail.com>
2020-10-29 17:49:03 +0200

    Spring Boot project created

---
+++ src/main/java/com/awscert/AwscertApplication.java
@@ -0,0 +1,13 @@
+package com.awscert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awscert/AwscertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awscert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
